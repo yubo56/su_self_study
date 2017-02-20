27cfa272bcc11bd88bcd73bf9538572e120e9e29
Yubo Su <yubo@blendlabs.com>
2017-02-19 18:28:31 -0800

    practiced some types

--- small_coding/haskell/0hello_world.hs
+++ small_coding/haskell/0hello_world.hs
@@ -1,3 +1,4 @@
-fac n = if n == 0 then 1 else n * fac (n - 1)
+fac 0 = 1
+fac n = n * fac ( n - 1 )
 
-main = print (fac 10)
+main = print ( show ( fac 10 ) ++ ", " ++ show ( fac 12 ) )
---
+++ small_coding/haskell/1types.hs
@@ -0,0 +1,34 @@
+-- lists
+arrLen :: [a] -> Integer
+arrLen [] = 0
+arrLen (x:xs) = 1 + arrLen(xs)
+-- main = print (arrLen [1, 2, 3])
+
+-- recursion
+collInt :: ([Char], Integer) -> ([Char], Integer)
+collInt (str, 1) = (str ++ "1", 1)
+collInt (str, val) =
+  if (mod val 2) == 0
+  then collInt (str ++ show val ++ ", ", (div val 2))
+  else collInt (str ++ show val ++ ", ", (3 * val + 1))
+
+collatz :: Integer -> [Char]
+collatz val = fst (collInt ("", val))
+-- main = print (collatz 11)
+
+-- recursive types
+data Tree a = Leaf a | Branch (Tree a) (Tree a)
+minTree :: (Tree a, a -> a -> a) -> a
+minTree (Leaf x, cmp) = x
+minTree (Branch x y, cmp) = cmp (minTree (x, cmp)) (minTree (y, cmp))
+
+intTree = Branch
+    (Leaf 3)
+    (Branch
+        (Branch
+            (Leaf 3)
+            (Leaf 5))
+        (Branch
+            (Leaf 2)
+            (Leaf 3)))
+-- main = print (minTree (intTree, min))
