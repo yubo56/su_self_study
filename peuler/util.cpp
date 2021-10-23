e943ec9370f4858839f1d9d716c16a08b6556e66
Yubo Su <yubo@blend.com>
2021-10-23 13:48:47 -0400

    make a prime number function, also make a root-finder for fun

---
+++ peuler/util.cpp
@@ -0,0 +1,24 @@
+/**
+ * test cases for util.hpp functions
+ */
+#include "util.hpp"
+#include <cstdio>
+#include <cmath>
+int main(int argc, const char *argv[])
+{
+    // int a = 2;
+    // int pow = 2;
+    // printf("%d\n", my_pow<int>(a, pow));
+
+    // printf("%f, %f\n", my_root<int>(23, 2, 4), pow(23, 1./2));
+    // printf("%f, %f\n", my_root<int>(23, 3, 4), pow(23, 1./3));
+
+    // bool is_prime[100];
+    // populate_primes(is_prime, 100);
+    // for (int i = 0; i < 100; i++)
+    // {
+    //     if (is_prime[i]) { printf("%d\n", i + 2); }
+    // }
+
+    return 0;
+}
---
+++ peuler/util.hpp
@@ -0,0 +1,82 @@
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+template <typename T>
+T my_pow(T a, int pow)
+{
+    T ret = 1;
+    for (int i = 0; i < pow; i++) { ret *= a; }
+    return ret;
+}
+
+// take root-th order root (e.g. root=2 => sqrt), for fun
+const double ROOT_TOL = 1e-10;
+template <typename T>
+double my_root(T a, int root, double guess)
+{
+    double left, right, guess_new;
+    // bracket the root first
+    if (my_pow(guess, root) > a)
+    {
+        right = guess;
+        left = guess / 2;
+        while (my_pow(left, root) > a)
+        {
+            right = left;
+            left /= 2;
+        }
+    }
+    else
+    {
+        right = guess * 2;
+        left = guess;
+        while (my_pow(right, root) < a)
+        {
+            left = right;
+            right *= 2;
+        }
+    }
+
+    // linearly extrapolate for next guess
+    int trials = 0;
+    while ((right - left) > ROOT_TOL)
+    {
+        if (trials < 10)
+        {
+            guess_new = (
+                (a - my_pow(left, root)) / (my_pow(right, root) - my_pow(left, root))
+                * (right - left)
+            ) + left + (trials % 2 == 0) ? ROOT_TOL * 10 : -ROOT_TOL * 10;
+        }
+        else
+        {
+            guess_new = (right + left) / 2;
+        }
+        if (my_pow(guess_new, root) > a)
+        {
+            right = guess_new;
+        }
+        else
+        {
+            left = guess_new;
+        }
+        trials++;
+    }
+    return right;
+}
+
+// get primes up through N + 2 (is_prime[i] = whether (i + 2) is prime)
+void populate_primes(bool *is_prime, int N)
+{
+    memset(is_prime, 1, N * sizeof(bool));
+    for (int i = 0; i < N; i++)
+    {
+        if (is_prime[i])
+        {
+            for (int j = 2; (i + 2) * j - 2 < N; j++)
+            {
+                is_prime[(i + 2) * j - 2] = false;
+            }
+        }
+    }
+}
