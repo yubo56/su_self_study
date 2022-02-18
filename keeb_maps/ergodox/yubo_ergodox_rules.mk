16d32b42421a83a15cdd746f377217517b36a6fe
Yubo Su <yubo@blend.com>
2022-02-18 03:10:16 -0500

    use stock ergodox options?

--- keeb_maps/ergodox/yubo_ergodox_rules.mk
+++ keeb_maps/ergodox/yubo_ergodox_rules.mk
@@ -12,19 +12,19 @@ BOOTLOADER = halfkay
 # Build Options
 #   change yes to no to disable
 #
-BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
-MOUSEKEY_ENABLE  = no   # Mouse keys
+BOOTMAGIC_ENABLE = no   # Enable Bootmagic Lite
+MOUSEKEY_ENABLE  = yes  # Mouse keys
 EXTRAKEY_ENABLE  = yes  # Audio control and System control
 CONSOLE_ENABLE   = no   # Console for debug
 COMMAND_ENABLE   = no   # Commands for debug and configuration
 CUSTOM_MATRIX    = lite # Custom matrix file for the ErgoDox EZ
-NKRO_ENABLE      = yes  # Enable N-Key Rollover
-UNICODE_ENABLE   = no   # Unicode
+NKRO_ENABLE      = yes           # Enable N-Key Rollover
+UNICODE_ENABLE   = no  # Unicode
 SWAP_HANDS_ENABLE= no   # Allow swapping hands of keyboard
 
 RGB_MATRIX_ENABLE = no # enable later
 RGB_MATRIX_DRIVER = IS31FL3731
-DEBOUNCE_TYPE = sym_eager_pk
+DEBOUNCE_TYPE = sym_eager_pr
 
 # project specific files
 SRC += matrix.c \
@@ -36,3 +36,5 @@ LAYOUTS = ergodox
 # Disable unsupported hardware
 AUDIO_SUPPORTED = no
 BACKLIGHT_SUPPORTED = no
+
+MOUSE_SHARED_EP = no
