f0a7b67cc631d1f3fced72425280c2cf1c83fa3b
Yubo Su <yubo56@gmail.com>
2024-05-28 12:03:37 -0400

    ugh forgot to update api key scrubbing

--- garmin/copy.sh
+++ garmin/copy.sh
@@ -2,5 +2,7 @@
 rsync -a ~/garmin-workspace/* . --exclude=bin
 sed -i '' 's/const appid.*/const appid = "";/g' YuboWatch/source/YuboWatchSDelegate.mc
 sed -i '' 's/const key.*/const key = "";/g' YuboWatch/source/YuboWatchSDelegate.mc
+sed -i '' 's/const appid.*/const appid = "";/g' YuboWatch_OWM/source/YuboOWMWatchSDelegate.mc
+sed -i '' 's/const key.*/const key = "";/g' YuboWatch_OWM/source/YuboOWMWatchSDelegate.mc
 sed -i '' 's/const appid.*/const appid = "";/g' HourlyWeathers/source/HourlyWeathersView.mc
 sed -i '' 's/const key.*/const key = "";/g' HourlyWeathers/source/HourlyWeathersView.mc
