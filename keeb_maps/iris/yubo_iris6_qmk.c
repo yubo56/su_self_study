eedec6ec15a4881334a24e40393724e3af806713
Yubo Su <yubo56@gmail.com>
2024-08-25 23:45:51 -0400

    clean up iris keymap a bit more

--- keeb_maps/iris/yubo_iris6_qmk.c
+++ keeb_maps/iris/yubo_iris6_qmk.c
@@ -1,5 +1,7 @@
 // Copyright 2023 Danny Nguyen (@nooges)
 // SPDX-License-Identifier: GPL-2.0-or-later
+//
+// Keycodes are @ ~/qmk_firmcare/docs/keycodes.md
 
 #include QMK_KEYBOARD_H
 
@@ -18,35 +20,35 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT
 (
-    KC_ESC  , KC_1 , KC_2 , KC_3 , KC_4     , KC_5                        , KC_6    , KC_7  , KC_8    , KC_9   , KC_0    , KC_NO   ,
-    KC_TAB  , KC_Q , KC_W , KC_E , KC_R     , KC_T                        , KC_Y    , KC_U  , KC_I    , KC_O   , KC_P    , KC_BSLS ,
-    MO(1)   , KC_A , KC_S , KC_D , KC_F     , KC_G                        , KC_H    , KC_J  , KC_K    , KC_L   , KC_SCLN , KC_ENT  ,
-    KC_LSFT , KC_Z , KC_X , KC_C , KC_V     , KC_B    , KC_MUTE , KC_NO   , KC_N    , KC_M  , KC_COMM , KC_DOT , KC_SLSH , KC_RSFT ,
-                                      MO(2) , KC_LGUI , KC_SPC  , KC_LCTL , KC_LALT , MO(3)
+    KC_ESC  , KC_1 , KC_2 , KC_3 , KC_4  , KC_5                        , KC_6    , KC_7  , KC_8    , KC_9   , KC_0    , KC_NO   ,
+    KC_TAB  , KC_Q , KC_W , KC_E , KC_R  , KC_T                        , KC_Y    , KC_U  , KC_I    , KC_O   , KC_P    , KC_BSLS ,
+    MO(1)   , KC_A , KC_S , KC_D , KC_F  , KC_G                        , KC_H    , KC_J  , KC_K    , KC_L   , KC_SCLN , KC_ENT  ,
+    KC_LSFT , KC_Z , KC_X , KC_C , KC_V  , KC_B    , KC_MUTE , KC_NO   , KC_N    , KC_M  , KC_COMM , KC_DOT , KC_SLSH , KC_RSFT ,
+                                   MO(2) , KC_LGUI , KC_SPC  , KC_LCTL , KC_LALT , MO(3)
 ),
     [1] = LAYOUT
 (
-    KC_NO   , KC_NO   , KC_NO  , KC_NO  , KC_NO   , KC_NO                      , KC_NO   , KC_NO   , KC_NO , KC_MINS , KC_EQL  , KC_NO   ,
-    KC_NO   , KC_NO   , KC_NO  , KC_NO  , KC_UP   , KC_NO                      , KC_RGHT , KC_NO   , KC_NO , KC_LBRC , KC_RBRC , KC_NO   ,
-    KC_TRNS , KC_HOME , KC_F11 , KC_END , KC_GRV  , KC_PGUP                    , KC_DEL  , KC_BSPC , KC_NO , KC_DOWN , KC_QUOT , KC_NO   ,
-    KC_LSFT , KC_NO   , KC_NO  , KC_NO  , KC_F6   , KC_PGDN    , KC_NO , KC_NO , KC_LEFT , KC_NO   , KC_F8 , KC_NO   , KC_NO   , KC_RSFT ,
-                                            KC_NO , KC_LGUI    , KC_NO , KC_NO , KC_RCTL , KC_RALT
+    KC_NO   , KC_NO   , KC_NO  , KC_NO  , KC_NO   , KC_NO                   , KC_NO   , KC_NO   , KC_NO , KC_MINS , KC_EQL  , KC_NO   ,
+    KC_NO   , KC_NO   , KC_NO  , KC_NO  , KC_UP   , KC_NO                   , KC_RGHT , KC_NO   , KC_NO , KC_LBRC , KC_RBRC , KC_NO   ,
+    KC_TRNS , KC_HOME , KC_F11 , KC_END , KC_GRV  , KC_PGUP                 , KC_DEL  , KC_BSPC , KC_NO , KC_DOWN , KC_QUOT , KC_NO   ,
+    KC_LSFT , KC_NO   , KC_NO  , KC_NO  , KC_F6   , KC_PGDN , KC_NO , KC_NO , KC_LEFT , KC_NO   , KC_F8 , KC_NO   , KC_NO   , KC_RSFT ,
+                                          KC_NO   , KC_LGUI , KC_NO , KC_NO , KC_RCTL , KC_RALT
 ),
     [2] = LAYOUT
 (
-    KC_NO   , KC_F1 , KC_F2 , KC_F3 , KC_F4      , KC_F5                      , KC_F6   , KC_F7    , KC_F8 , KC_F9  , KC_F10 , KC_NO ,
-    KC_NO   , KC_NO , KC_NO , KC_NO , KC_NO      , KC_NO                      , KC_NO   , KC_NO    , KC_NO , KC_F11 , KC_F12 , KC_NO ,
-    KC_NO   , KC_NO , KC_NO , KC_NO , KC_NO      , KC_NO                      , KC_NO   , KC_NO    , KC_NO , KC_NO  , KC_NO  , KC_NO ,
-    KC_LSFT , KC_NO , KC_NO , KC_NO , KC_NO      , KC_NO                      , KC_NO   , KC_NO    , KC_NO , KC_NO  , KC_NO  , KC_NO , KC_NO , KC_RSFT ,
-                                         KC_TRNS , KC_LGUI  , KC_LCTL , KC_NO , KC_RCTL , KC_RALT
-) ,
+    KC_NO   , KC_F1 , KC_F2 , KC_F3 , KC_F4      , KC_F5                     , KC_F6   , KC_F7    , KC_F8 , KC_F9  , KC_F10 , KC_NO ,
+    KC_NO   , KC_NO , KC_NO , KC_NO , KC_NO      , KC_NO                     , KC_NO   , KC_NO    , KC_NO , KC_F11 , KC_F12 , KC_NO ,
+    KC_NO   , KC_NO , KC_NO , KC_NO , KC_NO      , KC_NO                     , KC_NO   , KC_NO    , KC_NO , KC_NO  , KC_NO  , KC_NO ,
+    KC_LSFT , KC_NO , KC_NO , KC_NO , KC_NO      , KC_NO                     , KC_NO   , KC_NO    , KC_NO , KC_NO  , KC_NO  , KC_NO , KC_NO , KC_RSFT ,
+                                      KC_TRNS    , KC_LGUI , KC_LCTL , KC_NO , KC_RCTL , KC_RALT
+),
     [3] = LAYOUT
 (
-    KC_NO , RGB_TOG , RGB_RMOD , RGB_MOD , RGB_SPD , RGB_SPI                        , RGB_SAD , RGB_SAI , KC_NUM_LOCK , KC_PSLS , KC_PAST , KC_NO   ,
-    KC_NO , RGB_VAD , RGB_VAI  , RGB_HUD , RGB_HUI , RGB_SPD                        , RGB_SPI , KC_NO   , KC_P7       , KC_P8   , KC_P9   , KC_PMNS ,
-    KC_NO , KC_NO   , KC_NO    , KC_NO   , KC_NO   , KC_NO                          , KC_UP   , KC_DOWN , KC_P4       , KC_P5   , KC_P6   , KC_PPLS , KC_NO ,
-    KC_NO , QK_BOOT , KC_NO    , KC_NO   , KC_NO   , KC_NO      , KC_TRNS , KC_LEFT , KC_RGHT , KC_P1   , KC_P2       , KC_P3   , KC_PEQL ,
-                                             KC_NO , KC_NO      , KC_NO   , KC_TRNS , KC_VOLD , KC_VOLU
+    KC_NO , RGB_TOG , RGB_RMOD , RGB_MOD , RGB_SPD , RGB_SPI                   , RGB_SAD , RGB_SAI , KC_NUM_LOCK , KC_PSLS , KC_PAST , KC_NO   ,
+    KC_NO , RGB_VAD , RGB_VAI  , RGB_HUD , RGB_HUI , KC_NO                     , KC_NO   , KC_NO   , KC_P7       , KC_P8   , KC_P9   , KC_PMNS ,
+    KC_NO , KC_NO   , KC_NO    , KC_NO   , KC_NO   , KC_NO                     , KC_UP   , KC_DOWN , KC_P4       , KC_P5   , KC_P6   , KC_PPLS ,
+    KC_NO , KC_NO   , QK_BOOT  , KC_NO   , KC_NO   , KC_NO   , KC_NO , KC_TRNS , KC_LEFT , KC_RGHT , KC_P1       , KC_P2   , KC_P3   , KC_PEQL ,
+                                           KC_NO   , KC_NO   , KC_NO , KC_TRNS , KC_VOLD , KC_VOLU
 )
 };
 
